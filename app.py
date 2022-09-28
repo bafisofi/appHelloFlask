2f913b551ce085d58037485eb250c3081259b486
bafisofi <bafisofi@gmail.com>
2022-09-29 02:27:56 +0300

    third commit

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from flask import Flask, render_template, request, flash
 
 app= Flask(__name__)
-# app.secret_key = 'super secret key'
+app.secret_key = 'super secret key'
 
 
 @app.route("/hello")
